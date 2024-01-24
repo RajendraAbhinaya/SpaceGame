bf23d9456a15f6d4a724e647e6185141456f1678
Rajendra Abhinaya <abhinaya25152003@gmail.com>
2024-01-25 00:33:06 +0700

    Movement update

--- player.gd
+++ player.gd
@@ -1,13 +1,23 @@
 extends RigidBody2D
 
-var speed : float = 5.0
+var speed : float = 0.0
+var max_speed : float = 5.0
 var boost : float = 1000.0
 
 func _physics_process(delta):
 	look_at(get_global_mouse_position())
 	var direction = get_global_mouse_position() - global_position
 	if(Input.is_action_pressed("thrust")):
-		apply_force(direction * speed)
+		if speed < max_speed:
+			speed += delta
+		else:
+			speed = max_speed
+	else:
+		if speed > 0:
+			speed -= delta * 1.2
+		else:
+			speed = 0.0
+	apply_force(direction * speed)
 	
 	if(Input.is_action_just_pressed("thrust")):
 		if(!%Timer.is_stopped()):
